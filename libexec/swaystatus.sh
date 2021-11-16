c6852eda4aa265c2cee3e01bc9211913fc7c08a6
Andrew Pan <a@tny.town>
2021-11-16 00:57:13 -0600

    libexec/swaystatus: adjust for kernel 5.15
    changes to k10temp landed in 5.15, namely the rename of `Tdie`,
    the whole-die temperature reading, to `Tctl`. adjust accordingly.

--- libexec/swaystatus.sh
+++ libexec/swaystatus.sh
@@ -13,7 +13,7 @@ while true; do
     np=`n`
     echo \
         'load:' $(uptime | sed -E 's/.*load average: ([^ ]+),.*/\1/')'x' '|' \
-        'cpu:' $(sensors -j 'k10temp-pci-*' | jq '.. | .Tdie?.temp2_input | select(. != null) | floor')'C' '|' \
+        'cpu:' $(sensors -j 'k10temp-pci-*' | jq '.. | .Tctl?.temp1_input | select(. != null) | floor')'C' '|' \
         'mem:' $(free -mh | awk 'NR == 2 {print $3}') '|' \
         'net:' "$(echo $(( $np - $np_o )) | numfmt --to=iec-i --padding=5)" '|' \
         $(date +'%Y-%m-%d %H:%M:%S');
